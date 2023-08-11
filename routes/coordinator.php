2538de398b7fbe0b7fdc8b9edb03fce387b8f72d
RezwanaKarim <rezwanakarim13@gmail.com>
2023-08-11 20:38:42 +0600

    coordinator view-requestToPropose, groupRequestDetails, requestGroupDetails, requestGroupMembersDetails, student view -requestToCoordinator, changes in routes(coordiantor, student), studentController and CoordinatorRequestController, my group student button added for request

--- routes/coordinator.php
+++ routes/coordinator.php
@@ -13,6 +13,9 @@ Route::middleware(['CoordinatorAuth'])->group(function () {
     // Route::get('/coordinator/formedGroupsLists', [CoordinatorController::class, 'formedGroupsLists'])->name('coordinator.formedGroupsLists');
     Route::get('/coordinator/requests', [CoordinatorRequestController::class, 'requests'])->name('coordinator.requests');
     Route::get('/coordinator/requestDetails/{request}', [CoordinatorRequestController::class, 'requestDetails'])->name('coordinator.requestDetails');
+    Route::get('/coordinator/requestGroupDetails', [CoordinatorRequestController::class, 'requestGroupDetails'])->name('coordinator.requestGroupDetails');
+    Route::get('/coordinator/requestGroupMembersDetails', [CoordinatorRequestController::class, 'requestGroupMembersDetails'])->name('coordinator.requestGroupMembersDetails');
+    Route::get('/coordinator/requestToPropose', [CoordinatorRequestController::class, 'requestToPropose'])->name('coordinator.requestToPropose');
 
     Route::get('/coordinator/requests/group/{request}', [CoordinatorRequestController::class, 'formedGroupsLists'])->name('coordinator.formedGroupsLists');
     Route::post('/coordinator/requests/addToGroup', [CoordinatorRequestController::class, 'requestedStudentAddToGroup'])->name('coordinator.requestedStudentAddToGroup');
